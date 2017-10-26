101e0493be7022f44885fd28734ec20cd149ac64
Superworwolf <superworwolf@qq.com>
2017-10-26 09:22:23 +0800

    修复bug

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost:27017/Blog');
+mongoose.Promise = global.Promise;
+var db = mongoose.createConnection('mongodb://localhost:27017/Blog');
 
 //监听open事件
 db.once('open', function (callback) {
     console.log("数据库成功连接");
 });
-model.exports= db;
+module.exports = db;
